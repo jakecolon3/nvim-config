9c051c12c4cbde4b0f6a05d9fc6c6be025e660db
jake <minecrafterxxl3309@gmail.com>
2026-03-11 21:59:50 +0100

    fixed snippets loading for other languages

--- init.lua
+++ init.lua
@@ -656,12 +656,9 @@ require('lazy').setup({
           {
             'rafamadriz/friendly-snippets',
             config = function()
-              require('luasnip.loaders.from_vscode').lazy_load()
+              require('luasnip.loaders.from_vscode').lazy_load({ include = {'tex'}, })
             end,
           },
-        },
-        opts = {
-          include = {'tex'},
         },
       },
     },
